96ba6adda34878ae1793363cc93d42628e9cc9ff
Jennifer Sousa <jennifergaldinodesousa@gmail.com>
2022-01-28 20:21:02 -0300

    2801

---
+++ src/laço_de_repetição/Laço_de_Repetição.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laço_de_repetição;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class Laço_de_Repetição {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int contador = 0;
+       
+      do {
+          contador = contador + 1;
+          System.out.println(contador);
+      } while (contador < 10);
+       
+       
+    }
+    
+}
